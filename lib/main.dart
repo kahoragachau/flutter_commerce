0a8db75549f57b58bdfb824c59506e6d2042e924
kahoragachau <joelkahoragachau6@gmail.com>
2024-06-13 12:35:42 +0300

    feat: Wrap the Main file with ProviderScope giving widgets inside myapp access to the products provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_commerce/screens/home/home.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main(List<String> args) {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
