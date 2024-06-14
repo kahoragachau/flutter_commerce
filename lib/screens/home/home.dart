3824ba92911a0d285fdcc277196260bf12cca59f
kahoragachau <joelkahoragachau6@gmail.com>
2024-06-14 13:23:15 +0300

    feat: Add Product Provider

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_commerce/providers/products_providers.dart';
 import 'package:flutter_commerce/shared/cart_icon.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final allProducts = ref.watch(productsProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Garage Sale Product"),
@@ -15,7 +20,7 @@ class HomeScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: GridView.builder(
-          itemCount: 0,
+          itemCount: allProducts.length,
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             mainAxisExtent: 20,
@@ -26,6 +31,13 @@ class HomeScreen extends StatelessWidget {
             return Container(
               padding: const EdgeInsets.all(20),
               color: Colors.blueGrey.withOpacity(0.05),
+              child: Column(
+                children: [
+                  Image.asset(allProducts[index].image, width: 60, height: 60,),
+                  Text(allProducts[index].title),
+                  Text("${allProducts[index].price}"),
+                ],
+              ),
             );
           }
         ),
