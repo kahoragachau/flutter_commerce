a337daed2a9713039d8878a14bf48252b5043dbe
kahoragachau <joelkahoragachau6@gmail.com>
2024-06-18 23:47:27 +0300

    feat: Add Stateful Consumer Widget

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_commerce/providers/products_providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CartScreen extends StatefulWidget {
+class CartScreen extends ConsumerStatefulWidget {
   const CartScreen({super.key});
 
   @override
-  State<CartScreen> createState() => _CartScreenState();
+  ConsumerState<CartScreen> createState() => _CartScreenState();
 }
 
-class _CartScreenState extends State<CartScreen> {
+class _CartScreenState extends ConsumerState<CartScreen> {
 
   bool showCoupon = true;
 
   @override
   Widget build(BuildContext context) {
+
+    final cartProduct = ref.watch(reducedProductsProvider);
+    
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Cart'),
@@ -20,10 +25,23 @@ class _CartScreenState extends State<CartScreen> {
       ),
       body: Container(
         padding: const EdgeInsets.all(30),
-        child: const Column(
+        child: Column(
           children: [
             Column(
-              children: [],
+              children: cartProduct.map((e) {
+                return Container(
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  child: Row(
+                    children: [
+                      Image.asset(e.image, width: 60, height: 60),
+                      const SizedBox(width: 10),
+                      Text('${e.title}.....'),
+                      const Expanded(child: SizedBox()),
+                      Text('${e.price}'),
+                    ],
+                  ),
+                );
+              }).toList(),
             )
           ],
         ),
